88b0982d919aefb515e624934f209202e1831e6a
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-13 23:00:23 -0400

    freq tables , add superbad , fixed kconfig.iosched

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,7 +381,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 extern struct cpufreq_governor cpufreq_gov_minmax;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_minmax)
 extern struct cpufreq_governor cpufreq_gov_badass;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_badass)  
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_badass)
+extern struct cpufreq_governor cpufreq_gov_superbad;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_superbad)    
 #endif
 
 
